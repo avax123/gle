15c704b29b5e4518c5c2563dec42e8e1624c17be
avax123 <36860205+avax123@users.noreply.github.com>
2018-12-15 15:55:34 +0200

    Update ProductsController.php

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -30,7 +30,7 @@ class ProductsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *ssssssss
+     *ssssssss dildo
      */
     public function create()
     {
