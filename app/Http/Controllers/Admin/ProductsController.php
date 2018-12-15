1b14fc2ed8c381218ec733a6e45974e2711eddb8
Yuri Nalivaiko <YuriNalivaiko@gmail.com>
2018-12-15 16:00:33 +0200

    SAnya look here 2 plz, check user role ==admin

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -30,7 +30,7 @@ class ProductsController extends Controller
 
     /**
      * Show the form for creating a new resource.
-     *ssssssss dildo
+     *ssssssss dildo head
      */
     public function create()
     {
